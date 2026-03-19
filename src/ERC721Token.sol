bd1197dd513604b1ed15939ac64af257b7fe7349
calenorpobil <carlosbac3153@gmail.com>
2026-03-19 22:06:08 +0100

    ERC721

--- src/ERC721Token.sol
+++ src/ERC721Token.sol
@@ -5,7 +5,7 @@ import "./ERC165.sol";
 import "./interfaces/IREC721Receiver.sol";
 import "./interfaces/IERC721.sol";
 
-contract TokenERC721 is ERC165, IERC721 {
+contract ERC721Token is ERC165, IERC721 {
     mapping(uint256 => address) private _owners;
     mapping(address => uint256) private _balances;
     mapping(uint256 => address) private _tokenApprovals;
@@ -43,10 +43,6 @@ contract TokenERC721 is ERC165, IERC721 {
         return _tokenApprovals[tokenid];
     }
 
-    function _exists(uint256 tokenId) internal view returns (bool) {
-        return _owners[tokenId] != address(0);
-    }
-
     function setApprovalForAll(address operator, bool _approved) public virtual override {
         require(operator != msg.sender, "ERC721 ERROR: Operator address must be different from caller");
         _operatorApprovals[msg.sender][operator] = _approved;
@@ -57,31 +53,10 @@ contract TokenERC721 is ERC165, IERC721 {
         return _operatorApprovals[owner][operator];
     }
 
-    function transferFrom(address from, address to, uint256 tokenId) public virtual override {
-        require(_isApprovedOrOwner(msg.sender, tokenId), "ERC721 ERROR: Caller is not token owner nor approved");
-        _transfer(from, to, tokenId);
-    }
-
     function _isApprovedOrOwner(address spender, uint256 tokenId) internal view returns (bool) {
         require(_exists(tokenId), "ERC721 ERROR: Invalid token ID");
         address owner = ownerOf(tokenId);
         return (spender == owner || getApproved(tokenId) == spender || isApprovedForAll(owner, spender));
-    }
-
-    // El código a partir de aquí es autogenerado, se escribirá en siguientes commits. 
-
-    function _transfer(address from, address to, uint256 tokenId) internal {
-        require(ownerOf(tokenId) == from, "ERC721 ERROR: Transfer from incorrect owner");
-        require(to != address(0), "ERC721 ERROR: Transfer to the zero address");
-
-        // Clear approvals
-        _approve(address(0), tokenId);
-
-        _balances[from] -= 1;
-        _balances[to] += 1;
-        _owners[tokenId] = to;
-
-        emit Transfer(from, to, tokenId);
     }
 
     function safeTransferFrom(address from, address to, uint256 tokenId) public virtual override {
@@ -98,9 +73,56 @@ contract TokenERC721 is ERC165, IERC721 {
         require(_checkOnERC721Received(from, to, tokenId, _data), "ERC721 ERROR: Transfer to non ERC721Receiver implementer");
     }
 
-    function _checkOnERC721Received(address from, address to, uint256 tokenId, bytes memory _data) private returns (bool) {
+    function transferFrom(address from, address to, uint256 tokenId) public virtual override {
+        require(_isApprovedOrOwner(msg.sender, tokenId), 
+            "ERC721 ERROR: Caller is not token owner nor approved");
+        _transfer(from, to, tokenId);
+    }
+
+    function _transfer(address from, address to, uint256 tokenId) internal {
+        require(ownerOf(tokenId) == from, "ERC721 ERROR: Transfer from incorrect owner");
+        require(to != address(0), "ERC721 ERROR: Transfer to the zero address");
+
+        _beforeTokenTransfer(from, to, tokenId);
+        
+        _approve(address(0), tokenId);
+
+        _balances[from] -= 1;
+        _balances[to] += 1;
+        _owners[tokenId] = to;
+
+        emit Transfer(from, to, tokenId);
+    }
+
+    // El código a partir de aquí es autogenerado, se escribirá en siguientes commits. 
+
+
+    function _safeMint(address to, uint256 tokenId) public {
+        _safeMint(to, tokenId, "");
+    }
+
+    function _safeMint(address to, uint256 tokenId, bytes memory data) public {
+        _mint(to, tokenId);
+        require(_checkOnERC721Received(address(0), to, tokenId, data), 
+            "ERC721 ERROR: Transfer to non ERC721Receiver implementer");
+    }
+
+    function _mint(address to, uint256 tokenId) internal virtual {
+        require(to != address(0), "ERC721 ERROR: Mint to the zero address");
+        require(!_exists(tokenId), "ERC721 ERROR: Token already minted");
+        _beforeTokenTransfer(address(0), to, tokenId);
+
+        _balances[to] += 1;
+        _owners[tokenId] = to;
+
+        emit Transfer(address(0), to, tokenId);
+    }
+
+    function _checkOnERC721Received(address from, address to, uint256 tokenId, bytes memory _data) 
+    private returns (bool) {
         if (isContract(to)) {
-            try IERC721Receiver(to).onERC721Received(msg.sender, from, tokenId, _data) returns (bytes4 retval) {
+            try IERC721Receiver(to).onERC721Received(msg.sender, from, tokenId, _data) 
+            returns (bytes4 retval) {
                 return retval == IERC721Receiver.onERC721Received.selector;
             } catch (bytes memory reason) {
                 if (reason.length == 0) {
@@ -117,19 +139,22 @@ contract TokenERC721 is ERC165, IERC721 {
     }
 
     function isContract(address account) internal view returns (bool) {
-        return account.code.length > 0;
+        uint size;
+        assembly { 
+            size := extcodesize(account) 
+        }
+        return (size > 0);
     }
 
-    function mint(address to, uint256 tokenId) public {
-        require(to != address(0), "ERC721 ERROR: Mint to the zero address");
-        require(!_exists(tokenId), "ERC721 ERROR: Token already minted");
-
-        _balances[to] += 1;
-        _owners[tokenId] = to;
-
-        emit Transfer(address(0), to, tokenId);
+    function _exists(uint256 tokenId) internal view virtual returns (bool) {
+        return _owners[tokenId] != address(0);
     }
 
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId) internal virtual {
+        // Hook que se llama antes de cualquier transferencia de tokens. 
+        // Puede ser utilizado para implementar lógica adicional, como restricciones de transferencia o eventos personalizados.
+    }
+    
     function burn(uint256 tokenId) public {
         address owner = ownerOf(tokenId);
         require(msg.sender == owner, "ERC721 ERROR: Caller is not token owner");
